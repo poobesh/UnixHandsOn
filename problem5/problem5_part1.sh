02fc2cfe4421dcd6d47559601faf01f0da443304
Manish K <manish.k@accolite.com>
2022-11-13 00:02:45 +0530

    Formating for card limit

--- problem5/problem5_part1.sh
+++ problem5/problem5_part1.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+#function to convert card limit format
+usdfunction(){
+    #placing , after every three digits then placing $ in front and usd in end of string
+    usd=`echo $1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta' | sed 's/^/$/' | sed 's/$/ USD/'` 
+}
+
+#function to compare dates
 compare_dates(){
 	if [ $1 -gt $(date +%Y) ] #checking is expiry year is greater the current year
 	then
@@ -10,12 +17,14 @@ compare_dates(){
 		expiry_check=expired
 	fi 
 }
-while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6 rec_column7 rec_column8 rec_column9 rec_column10 rec_column11
+while IFS="," read -r card_type_code card_type_full_name issuing_bank card_number card_holder cvv issue_date expiry_date billing_date card_pin card_limit
 do
-	ex_year="${rec_column8:3:6}" #extracting expiry year
-	ex_month=$[$((10#${rec_column8:0:2}))] #extracting expiry mounth and changing base to 10 (to remove value too great for base)
+	ex_year="${expiry_date:3:6}" #extracting expiry year
+	ex_month=$[$((10#${expiry_date:0:2}))] #extracting expiry mounth and changing base to 10 (to remove value too great for base)
 	compare_dates $ex_year $ex_month #call for compare dates function with arguemnts expiry year and expiry mounth
-    echo $ex_year $ex_month
-    echo $expiry_check
+    #echo $ex_year $ex_month
+    #echo $expiry_check
+    usdfunction $card_limit
+    echo $usd
 
 done < <((tail -n +2 100\ CC\ Records.csv))
