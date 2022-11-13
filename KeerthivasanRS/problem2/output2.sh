f5f41a24499bd24cae479cd6b609cf563b764748
Keerthivasan S <keerthi.s@accolite.com>
2022-11-14 00:06:31 +0530

    solutions

---
+++ KeerthivasanRS/problem2/output2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+echo "Current date and time is: $(date)"
+echo "Current date is: $(date +"%D")"
+echo "Current time is: $(date +"%T")"
+echo "Username is: $(whoami)"
+echo "Current working directory is: $(pwd)"
