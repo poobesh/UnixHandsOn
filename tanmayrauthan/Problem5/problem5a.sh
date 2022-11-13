5c9fafee8671b9e00196e471a90d0d2b69fd263c
Tanmay Rauthan <tanmay.rauthan@accolite.com>
2022-11-13 22:26:05 +0530

    Currency format change Problem5_a

--- tanmayrauthan/Problem5/problem5a.sh
+++ tanmayrauthan/Problem5/problem5a.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 currentMonth=$(date +"%m")
 currentYear=$(date +"%Y")
-
+convertfun(){
+    creditlim=`echo $1 | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta' | sed 's/^/$/' | sed 's/$/ USD/'`
+}
 while IFS="," read -r cardCode cardName issuingBank cardNumber cardHolderName cvv issueDate expiryDate billingDate cardPIN creditLimit
 do
+    convertfun $creditLimit
 	if [ -d $cardName ]
 	then
 		if [ -d $cardName/$issuingBank ]
@@ -42,6 +45,6 @@ do
 
 	fileName="$cardNumber.$isActive.txt"
 	
-	echo -e "Card Type Code: $cardCode\nCard Type Full Name: $cardName\nIssuing Bank: $issuingBank\nCard Number: $cardNumber\nCard Holder's Name: $cardHolderName\nCVV/Cvv2: $cvv\nIssue Date: $issueDate\nExpiry Date: $expiryDate\nBilling Date: $billingDate\nCard PIN: $cardPIN\nCredit Limit: $ $creditLimit USD\n" > "$cardName/$issuingBank/$fileName"
+	echo -e "Card Type Code: $cardCode\nCard Type Full Name: $cardName\nIssuing Bank: $issuingBank\nCard Number: $cardNumber\nCard Holder's Name: $cardHolderName\nCVV/Cvv2: $cvv\nIssue Date: $issueDate\nExpiry Date: $expiryDate\nBilling Date: $billingDate\nCard PIN: $cardPIN\nCredit Limit: $creditlim\n" > "$cardName/$issuingBank/$fileName"
 
 done < <(tail -n +2 Records.csv)
