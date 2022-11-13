cc9a11e29a5fd70ba342ef4db8454e49144c2d89
Yousha Samir Gharpure <yousha.gharpure@accolite.com>
2022-11-14 00:03:10 +0530

    Submission

---
+++ yousha/problem1/july.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#prints pattern july from all files in directory "abc"
+grep july ./abc/*
+
+#case insensitive
+#grep -i july ./abc/*
+
+#prints pattern july from all files in current directory
+#grep july *
---
+++ yousha/problem2/current.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Current date: $(date +'%m/%d/%Y')"
+echo "Current time: $(date +"%T")"
+echo "Current Username: `$USER`"
+echo "Current working directory: $PWD"
